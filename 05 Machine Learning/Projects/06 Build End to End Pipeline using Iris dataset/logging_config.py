66007050cf73097f7b7b4b8c4fc90d957d37af87
thasvithu <thasvithu7@gmail.com>
2025-03-17 22:07:08 +0530

    loggging implemented

---
+++ 05 Machine Learning/Projects/06 Build End to End Pipeline using Iris dataset/logging_config.py
@@ -0,0 +1,10 @@
+import logging
+
+# Configure the root logger
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S %p'
+)
+
+logger = logging.getLogger(__name__)
--- 05 Machine Learning/Projects/06 Build End to End Pipeline using Iris dataset/template.py
+++ 05 Machine Learning/Projects/06 Build End to End Pipeline using Iris dataset/template.py
@@ -1,34 +1,6 @@
+
 import os
 from pathlib import Path
-import logging
 
-# Configure logging at the start
-logging.basicConfig(
-    filename='app.log',
-    filemode='w',
-    level=logging.DEBUG,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    datefmt='%Y/%m/%d %H:%M:%S %p'
-)
+project_name = "iris-ml-app"
 
-def test():
-    logging.debug('This will get logged')   
-    logging.warning('This will get logged')
-    logging.info('This will not get logged')
-    logging.error('This will get logged')
-    logging.critical('This will get logged')
-    logging.debug('This will get logged')   
-    logging.warning('This will get logged')
-    logging.info('This will not get logged')
-    logging.error('This will get logged')
-    logging.critical('This will get logged')
-    logging.debug('This will get logged')   
-    logging.warning('This will get logged')
-    logging.info('This will not get logged')
-    logging.error('This will get logged')
-    logging.critical('This will get logged')
-
-
-
-if __name__ == '__main__':
-    test()
